a89584039a65ee682d6b528934d5bcccf72bb836
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2022-02-10 21:01:16 -0300

    iterações de js e melhoramento da acessibilidade do modal

--- assets/js/dialog.js
+++ assets/js/dialog.js
@@ -1,19 +1,37 @@
 // Variáveis
-var btnAbreDialog = document.querySelector('#abreDialog');
-var dialog = document.querySelector('.dialogNewsletter');
-var dialogBody = document.querySelector('.dialogNewsletter-body');
-var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
+let conteudoForaDiaLog = document.queryCommandValue('#conteudoForaDialog')
+let btnAbreDialog = document.querySelector("#abreDialog")
+let dialog = document.querySelector('.dialogNewsletter')
+let dialogBody = document.querySelector('.dialogNewsletter-body')
+let dialogOverlay = document.querySelector('.dialogNewsletter-overlay')
 
+btnAbreDialog.style.display = "block"
 
 // Quando abrir a dialog...
 btnAbreDialog.addEventListener('click', function() {
-  dialog.classList.add('dialogNewsletter--aberto');
+  dialog.classList.add('dialogNewsletter--aberto')
+  document.querySelector('.dialogNewsletter-label').focus()
+  // conteudoForaDiaLog.inert = true
+  conteudoForaDialog.setAttribute("inert", true)
+  document.querySelector('.conteudo-dialog').classList.add("blur-modal")
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
-  dialog.classList.remove('dialogNewsletter--aberto');     
+  dialog.classList.remove('dialogNewsletter--aberto')   
+  // conteudoForaDiaLog.inert = false
+  conteudoForaDialog.removeAttribute("inert")
+  document.querySelector('.conteudo-dialog').classList.remove("blur-modal")
+  document.addEventListener('keyup', function(evento) {
+    if(evento.keyCode == 27) {
+      fechandoDialog()
+    }
+  })
+
+  btnAbreDialog.focus()
 }
 
 // Listeners
 document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fechandoDialog);
+dialogOverlay.addEventListener('click', fechandoDialog)
+
